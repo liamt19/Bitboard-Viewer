9607664dbea2d034cc4c4fc9cc5831810e28a226
Liam McGuire <liam.t.mcguire@gmail.com>
2025-03-05 20:02:34 -0500

    shogi moment

--- js/app.js
+++ js/app.js
@@ -18,14 +18,18 @@ $(document).ready(function() {
     $('#layoutRadio3').click(() => changeLayout(3));
     $('#layoutRadio4').click(() => changeLayout(4));
     
-    $('#decBitboard1').keyup(() => decKeyUp($('#bitboard1'), $('#decBitboard1'), $('#hexBitboard1'), $('#binBitboard1')));
-    $('#hexBitboard1').keyup(() => hexKeyUp($('#bitboard1'), $('#decBitboard1'), $('#hexBitboard1'), $('#binBitboard1')));
-    $('#binBitboard1').keyup(() => binKeyUp($('#bitboard1'), $('#decBitboard1'), $('#hexBitboard1'), $('#binBitboard1')));
-    
-    $('#decBitboard2').keyup(() => decKeyUp($('#bitboard2'), $('#decBitboard2'), $('#hexBitboard2'), $('#binBitboard2')));
-    $('#hexBitboard2').keyup(() => hexKeyUp($('#bitboard2'), $('#decBitboard2'), $('#hexBitboard2'), $('#binBitboard2')));
-    $('#binBitboard2').keyup(() => binKeyUp($('#bitboard2'), $('#decBitboard2'), $('#hexBitboard2'), $('#binBitboard2')));
-    
+    $('#decBitboard1').keyup(() => decKeyUp($('#bitboard1'), $('#decBitboard1'), $('#hexBitboard1'), $('#binBitboard1'), $('#upperBitboard1'), $('#lowerBitboard1')));
+    $('#hexBitboard1').keyup(() => hexKeyUp($('#bitboard1'), $('#decBitboard1'), $('#hexBitboard1'), $('#binBitboard1'), $('#upperBitboard1'), $('#lowerBitboard1')));
+    $('#binBitboard1').keyup(() => binKeyUp($('#bitboard1'), $('#decBitboard1'), $('#hexBitboard1'), $('#binBitboard1'), $('#upperBitboard1'), $('#lowerBitboard1')));
+    $('#upperBitboard1').keyup(() => upperKeyUp($('#bitboard1'), $('#decBitboard1'), $('#hexBitboard1'), $('#binBitboard1'), $('#upperBitboard1'), $('#lowerBitboard1')));
+    $('#lowerBitboard1').keyup(() => lowerKeyUp($('#bitboard1'), $('#decBitboard1'), $('#hexBitboard1'), $('#binBitboard1'), $('#upperBitboard1'), $('#lowerBitboard1')));
+
+    $('#decBitboard2').keyup(() => decKeyUp($('#bitboard2'), $('#decBitboard2'), $('#hexBitboard2'), $('#binBitboard2'), $('#upperBitboard2'), $('#lowerBitboard2')));
+    $('#hexBitboard2').keyup(() => hexKeyUp($('#bitboard2'), $('#decBitboard2'), $('#hexBitboard2'), $('#binBitboard2'), $('#upperBitboard2'), $('#lowerBitboard2')));
+    $('#binBitboard2').keyup(() => binKeyUp($('#bitboard2'), $('#decBitboard2'), $('#hexBitboard2'), $('#binBitboard2'), $('#upperBitboard2'), $('#lowerBitboard2')));
+    $('#upperBitboard2').keyup(() => upperKeyUp($('#bitboard1'), $('#decBitboard1'), $('#hexBitboard1'), $('#binBitboard1'), $('#upperBitboard1'), $('#lowerBitboard1')));
+    $('#lowerBitboard2').keyup(() => lowerKeyUp($('#bitboard1'), $('#decBitboard1'), $('#hexBitboard1'), $('#binBitboard1'), $('#upperBitboard1'), $('#lowerBitboard1')));
+
     $('#fillBitboard1').click(() => fillBitboard($('#decBitboard1')));
     $('#fillBitboard2').click(() => fillBitboard($('#decBitboard2')));
     
@@ -51,12 +55,12 @@ $(document).ready(function() {
 });
 
 function generateLayout(layout, variant) {
-    for (var y = 0; y < 8; y++) {
+    for (var y = 0; y < 9; y++) {
         var row = $(document.createElement('div')).prop({
             class: 'layout-row'
         });
         
-        for (var x = 0; x < 8; x++) {
+        for (var x = 0; x < 9; x++) {
             var value = getselectedLayoutByXY(variant, x, y);
             if (value < 10) {
                 value = '0' + value;
@@ -78,7 +82,7 @@ function generateBitboard(bitboard, decTextbox, readOnly) {
 		});
 	}
 	
-	for (var y = 0; y < 8; y++) {
+	for (var y = 0; y < 9; y++) {
 		var row = $(document.createElement('div')).prop({
 			class: 'bitboard-row'
 		});
@@ -96,7 +100,7 @@ function generateBitboard(bitboard, decTextbox, readOnly) {
 		}
 		
 		// Buttons to fill columns
-		const files =  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+		const files =  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
 		if (!readOnly) {
 			var colbutton = $(document.createElement('button')).prop({
 				type: 'colbutton',
@@ -110,8 +114,8 @@ function generateBitboard(bitboard, decTextbox, readOnly) {
 		}
 		
 		// Checkboxes
-		for (var x = 0; x < 8; x++) {
-			var value = x + y * 8;
+		for (var x = 0; x < 9; x++) {
+			var value = x + y * 9;
 			var checkbox = $(document.createElement('input')).prop({
 				type: 'checkbox',
 				value: value,
@@ -169,9 +173,9 @@ function changeLayout(variant) {
 }
 
 function refreshValuesAfterLayoutChange() {
-    decKeyUp($('#bitboard1'), $('#decBitboard1'), $('#hexBitboard1'), $('#binBitboard1'));
-    decKeyUp($('#bitboard2'), $('#decBitboard2'), $('#hexBitboard2'), $('#binBitboard2'));
-    decKeyUp($('#bitboard3'), $('#decBitboard3'), $('#hexBitboard3'), $('#binBitboard3'));
+    decKeyUp($('#bitboard1'), $('#decBitboard1'), $('#hexBitboard1'), $('#binBitboard1'), $('#upperBitboard1'), $('#lowerBitboard1'));
+    decKeyUp($('#bitboard2'), $('#decBitboard2'), $('#hexBitboard2'), $('#binBitboard2'), $('#upperBitboard2'), $('#lowerBitboard2'));
+    decKeyUp($('#bitboard3'), $('#decBitboard3'), $('#hexBitboard3'), $('#binBitboard3'), $('#upperBitboard3'), $('#lowerBitboard3'));
 }
 
 function doOperation(operation) {
@@ -183,38 +187,72 @@ function doOperation(operation) {
     updateBitboard($('#bitboard3'), result);
 }
 
-function decKeyUp(bitboard, decTextbox, hexTextbox, binTextbox) {
+function decKeyUp(bitboard, decTextbox, hexTextbox, binTextbox, upperTextBox, lowerTextBox) {
     var bigIntValue = BigInt(decTextbox.val());
-    hexTextbox.val('0x' + bigIntValue.toString(16));
+    hexTextbox.val('0x' + itoh(bigIntValue));
     binTextbox.val('0b' + bigIntValue.toString(2));
+
+    upperTextBox.val('0x' + itoh(upperBits(bigIntValue)));
+    lowerTextBox.val('0x' + itoh(lowerBits(bigIntValue)));
     
     updateBitboard(bitboard, bigIntValue);
 }
 
-function hexKeyUp(bitboard, decTextbox, hexTextbox, binTextbox) {
+function hexKeyUp(bitboard, decTextbox, hexTextbox, binTextbox, upperTextBox, lowerTextBox) {
     var bigIntValue = BigInt(hexTextbox.val());
     decTextbox.val(bigIntValue.toString(10));
     binTextbox.val('0b' + bigIntValue.toString(2));
+
+    upperTextBox.val('0x' + itoh(upperBits(bigIntValue)));
+    lowerTextBox.val('0x' + itoh(lowerBits(bigIntValue)));
     
     updateBitboard(bitboard, bigIntValue);
 }
 
-function binKeyUp(bitboard, decTextbox, hexTextbox, binTextbox) {
+function binKeyUp(bitboard, decTextbox, hexTextbox, binTextbox, upperTextBox, lowerTextBox) {
     var bigIntValue = BigInt(binTextbox.val());
     decTextbox.val(bigIntValue.toString(10));
-    hexTextbox.val('0x' + bigIntValue.toString(16));
+    hexTextbox.val('0x' + itoh(bigIntValue));
+
+    upperTextBox.val('0x' + itoh(upperBits(bigIntValue)));
+    lowerTextBox.val('0x' + itoh(lowerBits(bigIntValue)));
+    
+    updateBitboard(bitboard, bigIntValue);
+}
+
+function upperKeyUp(bitboard, decTextbox, hexTextbox, binTextbox, upperTextBox, lowerTextBox) {
+    var bigIntValue = make128(BigInt(upperTextBox.val()), BigInt(lowerTextBox.val()))
+    decTextbox.val(bigIntValue.toString(10));
+    hexTextbox.val('0x' + itoh(bigIntValue));
+    binTextbox.val('0b' + bigIntValue.toString(2));
+
+    lowerTextBox.val('0x' + itoh(lowerBits(bigIntValue)));
+    
+    updateBitboard(bitboard, bigIntValue);
+}
+
+function lowerKeyUp(bitboard, decTextbox, hexTextbox, binTextbox, upperTextBox, lowerTextBox) {
+    var bigIntValue = make128(BigInt(upperTextBox.val()), BigInt(lowerTextBox.val()))
+    decTextbox.val(bigIntValue.toString(10));
+    hexTextbox.val('0x' + itoh(bigIntValue));
+    binTextbox.val('0b' + bigIntValue.toString(2));
+
+    upperTextBox.val('0x' + itoh(upperBits(bigIntValue)));
     
     updateBitboard(bitboard, bigIntValue);
 }
 
 function updateReadOnlyTextboxes(value) {
     $('#decBitboard3').val(value.toString(10));
-    $('#hexBitboard3').val('0x' + value.toString(16));
+    $('#hexBitboard3').val('0x' + itoh(value));
     $('#binBitboard3').val('0b' + value.toString(2));
+
+    $('#lowBitboard3').val('0x' + itoh(lowerBits(value)));
+    $('#highBitboard3').val('0x' + itoh(upperBits(value)));
 }
 
 function updateBitboard(bitboard, value) {
-    for (var index = 0; index < 64; index++) {
+    for (var index = 0; index < 81; index++) {
         var bit = value & 1n;
         value = value >> 1n;
         
@@ -236,10 +274,10 @@ function bitboardCheckboxClick(bitboard, decTextbox, index) {
 
 function rowClick(bitboard, decTextbox, rank){
     // Magic number is a fully filled 8th rank
-    var toprow = 18374686479671623680n;
+    var toprow = 2413129272746388704198656n;
     // Inverse the shiftvalue for different layouts
     var shiftval = BigInt(calcRowShiftValue(selectedLayout, rank));
-    var row = toprow >> (shiftval * 8n);
+    var row = toprow >> (shiftval * 9n);
     // OR the existing field and the newly filled row
     var newvalue = BigInt(decTextbox.val()) | row;
 
@@ -254,12 +292,12 @@ function rowClick(bitboard, decTextbox, rank){
 }
 
 function colClick(bitboard, decTextbox, file){
-    const files =  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+    const files =  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
     file = BigInt(files.indexOf(file));
     // Magic number is a fully filled H file
-    var rightcol = 9259542123273814144n;
+    var rightcol = 2365803952279117758720n
     // Inverse the shiftvalue for different layouts
-    var shiftval = calcColShiftValue(selectedLayout, 7n - file);
+    var shiftval = calcColShiftValue(selectedLayout, 8n - file);
     var col =  rightcol >> shiftval;
     // OR the existing field and the newly filled col
     var newvalue = BigInt(decTextbox.val()) | col;
@@ -275,7 +313,7 @@ function colClick(bitboard, decTextbox, file){
 }
 
 function fillBitboard(decTextbox) {
-    decTextbox.val('18446744073709551615');
+    decTextbox.val('2417851639229258349412351');
     refreshValuesAfterLayoutChange();
 }
 
@@ -286,7 +324,7 @@ function clearBitboard(decTextbox) {
 
 function shlBitboard(decTextbox) {
     var value = BigInt(decTextbox.val());
-    var lastBitValue = value & (1n << 63n);
+    var lastBitValue = value & (1n << 80n);
     if(lastBitValue != 0n) {
         value = value & ~lastBitValue; 
     }
@@ -299,7 +337,7 @@ function shlBitboard(decTextbox) {
 
 function shrBitboard(decTextbox) {
     var value = BigInt(decTextbox.val());
-    value = value >> 1n & ~(1n << 63n);
+    value = value >> 1n & ~(1n << 80n);
     decTextbox.val(value);
     
     refreshValuesAfterLayoutChange();
@@ -307,7 +345,7 @@ function shrBitboard(decTextbox) {
 
 function notBitboard(decTextbox) {
     var value = BigInt(decTextbox.val());
-    value = 18446744073709551615n - value;
+    value = 2417851639229258349412351n - value;
     decTextbox.val(value);
     
     refreshValuesAfterLayoutChange();
@@ -315,33 +353,49 @@ function notBitboard(decTextbox) {
 
 function getselectedLayoutByXY(variant, x, y) {
     switch (variant) {
-        case 1: return 63 - (7 - x + y * 8);
-        case 2: return 63 - (x + y * 8);
-        case 3: return x + y * 8;
-        case 4: return 7 - x + y * 8;
+        case 1: return 80 - (8 - x + y * 9);
+        case 2: return 80 - (x + y * 9);
+        case 3: return x + y * 9;
+        case 4: return 8 - x + y * 9;
     }
     
     return 0;
 }
 
 function getselectedLayoutByIndex(variant, index) {
-    return getselectedLayoutByXY(variant, index % 8, Math.floor(index / 8));
+    return getselectedLayoutByXY(variant, index % 9, Math.floor(index / 9));
 }
 
 function calcRowShiftValue(variant, value) {
     switch (variant) {
         case 1: return value;
         case 2: return value;
-        case 3: return 7 - value;
-        case 4: return 7 - value;
+        case 3: return 8 - value;
+        case 4: return 8 - value;
     }
 }
 
 function calcColShiftValue(variant, value) {
     switch (variant) {
         case 1: return value;
-        case 2: return 7n - value;
+        case 2: return 8n - value;
         case 3: return value;
-        case 4: return 7n - value;
+        case 4: return 8n - value;
     }
+}
+
+function lowerBits(value) {
+    return value & 18446744073709551615n;
+}
+
+function upperBits(value) {
+    return ((value >> 64n) & 18446744073709551615n);
+}
+
+function make128(upper, lower) {
+    return (upper << 64n) | lower;
+}
+
+function itoh(value) {
+    return value.toString(16).toUpperCase();
 }
